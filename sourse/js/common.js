4a3cf5eefb0c7cb520f1c72cd202c417844ffe79
Pavel <wol1414@gmail.com>
2021-06-05 16:13:09 +0300

    Initial commit

---
+++ sourse/js/common.js
@@ -0,0 +1,266 @@
+let div = document.createElement('div');
+
+div.style.overflowY = 'scroll';
+div.style.width = '50px';
+div.style.height = '50px';
+
+// мы должны вставить элемент в документ, иначе размеры будут равны 0
+document.body.append(div);
+
+let scrollWidth = div.offsetWidth - div.clientWidth;
+let root = document.documentElement;
+root.style.setProperty('--spacing-end', scrollWidth + 'px');
+div.remove();
+const JSCCommon = {
+
+	btnToggleMenuMobile: [].slice.call(document.querySelectorAll(".toggle-menu-mobile--js")),
+	menuMobile: document.querySelector(".menu-mobile--js"),
+	menuMobileLink: [].slice.call(document.querySelectorAll(".menu-mobile--js ul li a")),
+
+	modalCall() {
+		const link = ".link-modal-js";
+		$(link).fancybox({
+			arrows: false,
+			infobar: false,
+			touch: false,
+			type: 'inline',
+			autoFocus: false,
+			i18n: {
+				en: {
+					CLOSE: "Закрыть",
+					NEXT: "Вперед",
+					PREV: "Назад",
+					// PLAY_START: "Start slideshow",
+					// PLAY_STOP: "Pause slideshow",
+					// FULL_SCREEN: "Full screen",
+					// THUMBS: "Thumbnails",
+					// DOWNLOAD: "Download",
+					// SHARE: "Share",
+					// ZOOM: "Zoom"
+				},
+			},
+			// beforeLoad: function () {
+			// 	root.style.setProperty('--spacing-end', scrollWidth + 'px');
+			// },
+			// afterClose: function () {
+			// 	root.style.setProperty('--spacing-end', null);
+			// },
+		});
+		$(".modal-close-js").click(function () {
+			$.fancybox.close();
+		})
+		$.fancybox.defaults.backFocus = false;
+		const linkModal = document.querySelectorAll(link);
+		function addData() {
+			linkModal.forEach(element => {
+				element.addEventListener('click', () => {
+					let modal = document.querySelector(element.getAttribute("href"));
+					const data = element.dataset;
+
+					function setValue(val, elem) {
+						if (elem && val) {
+							const el = modal.querySelector(elem)
+							el.tagName == "INPUT"
+								? el.value = val
+								: el.innerHTML = val;
+							// console.log(modal.querySelector(elem).tagName)
+						}
+					}
+					setValue(data.title, '.ttu');
+					setValue(data.text, '.after-headline');
+					setValue(data.btn, '.btn');
+					setValue(data.order, '.order');
+				})
+			})
+		}
+		if (linkModal) addData();
+	},
+	// /modalCall
+	toggleMenu() {
+		const toggle = this.btnToggleMenuMobile;
+		const menu = this.menuMobile;
+		document.addEventListener("click", function (event) {
+			const toggleEv = event.target.closest(".toggle-menu-mobile--js");
+			if (!toggleEv) return;
+			toggle.forEach(el => el.classList.toggle("on"));
+			menu.classList.toggle("active");
+			[document.body, document.querySelector('html')].forEach(el => el.classList.toggle("fixed")); 
+		}, { passive: true });
+	},
+	closeMenu() {
+		let menu = this.menuMobile;
+		if (!menu) return;
+		if (menu.classList.contains("active")) {
+			this.btnToggleMenuMobile.forEach(element => element.classList.remove("on"));
+			this.menuMobile.classList.remove("active");
+			[document.body, document.querySelector('html')].forEach(el => el.classList.remove("fixed")); 
+		}
+
+	},
+	mobileMenu() {
+		if (!this.menuMobileLink) return;
+		this.toggleMenu();
+		document.addEventListener('mouseup', (event) => {
+			let container = event.target.closest(".menu-mobile--js.active"); // (1)
+			let link = event.target.closest(".navMenu__link"); // (1)
+			if (!container || link) this.closeMenu();
+		}, { passive: true });
+
+		window.addEventListener('resize', () => {
+			if (window.matchMedia("(min-width: 992px)").matches) this.closeMenu();
+		}, { passive: true });
+	},
+	// /mobileMenu
+
+	// tabs  .
+	tabscostume(tab) {
+	 
+		$('.' + tab + '__caption').on('click', '.' + tab + '__btn:not(.active)', function (e) {
+			$(this)
+				.addClass('active').siblings().removeClass('active')
+				.closest('.' + tab).find('.' + tab + '__content').hide().removeClass('active')
+				.eq($(this).index()).fadeIn().addClass('active');
+
+		});
+
+	},
+	// /tabs
+
+	inputMask() {
+		// mask for input
+		let InputTel = [].slice.call(document.querySelectorAll('input[type="tel"]'));
+		InputTel.forEach(element => element.setAttribute("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}"));
+		Inputmask("+9(999)999-99-99").mask(InputTel);
+	},
+	// /inputMask
+	ifie() {
+		var isIE11 = !!window.MSInputMethodContext && !!document.documentMode;
+		if (isIE11) {
+			document.body.insertAdjacentHTML("beforeend", '<div class="browsehappy">	<p class=" container">К сожалению, вы используете устаревший браузер. Пожалуйста, <a href="http://browsehappy.com/" target="_blank">обновите ваш браузер</a>, чтобы улучшить производительность, качество отображаемого материала и повысить безопасность.</p></div>');
+		}
+	},
+
+	heightwindow() {
+		// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+		let vh = window.innerHeight * 0.01;
+		// Then we set the value in the --vh custom property to the root of the document
+		document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+		// We listen to the resize event
+		window.addEventListener('resize', () => {
+			// We execute the same script as before
+			let vh = window.innerHeight * 0.01;
+			document.documentElement.style.setProperty('--vh', `${vh}px`);
+		}, { passive: true });
+	},
+	animateScroll() {
+
+		$(document).on('click', " .top-nav li a, .scroll-link", function () {
+			const elementClick = $(this).attr("href");
+			const destination = $(elementClick).offset().top;
+
+			$('html, body').animate({ scrollTop: destination }, 1100);
+
+			return false;
+		});
+	},
+	getCurrentYear(el) {
+		let now = new Date();
+		let currentYear = document.querySelector(el);
+		if (currentYear) currentYear.innerText = now.getFullYear();
+	}
+};
+const $ = jQuery;
+
+function eventHandler() {
+	JSCCommon.ifie();
+	JSCCommon.modalCall();
+	JSCCommon.tabscostume('tabs');
+	JSCCommon.mobileMenu();
+	JSCCommon.inputMask(); 
+	JSCCommon.heightwindow();
+	JSCCommon.animateScroll();
+
+	// JSCCommon.CustomInputFile(); 
+	var x = window.location.host;
+	let screenName;
+	screenName = document.body.dataset.bg;
+	if (screenName && x.includes("localhost:30")) {
+		document.body.insertAdjacentHTML("beforeend", `<div class="pixel-perfect" style="background-image: url(screen/${screenName});"></div>`);
+	}
+
+
+	function setFixedNav() {
+		let topNav = document.querySelector('.top-nav  ');
+		if (!topNav) return;
+		window.scrollY > 0
+			? topNav.classList.add('fixed')
+			: topNav.classList.remove('fixed');
+	}
+
+	function whenResize() {
+		setFixedNav();
+	}
+
+	window.addEventListener('scroll', () => {
+		setFixedNav();
+
+	}, { passive: true })
+	window.addEventListener('resize', () => {
+		whenResize();
+	}, { passive: true });
+
+	whenResize();
+
+
+	let defaultSl = {
+		spaceBetween: 0,
+		lazy: {
+			loadPrevNext: true,
+			loadPrevNextAmount: 3,
+		},
+		watchOverflow: true,
+		spaceBetween: 0,
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: ' .swiper-pagination',
+			type: 'bullets',
+			clickable: true,
+			// renderBullet: function (index, className) {
+			// 	return '<span class="' + className + '">' + (index + 1) + '</span>';
+			// }
+		},
+	}
+
+	const swiperAll = new Swiper('.sBanners__slider--js', {
+		// slidesPerView: 5,
+		...defaultSl,
+		slidesPerView: 1,
+	});
+
+	const swiper4 = new Swiper('.slider-js', {
+		// slidesPerView: 5,
+		...defaultSl,
+		slidesPerView: 1,
+		spaceBetween: 20,
+	});
+	// modal window
+
+};
+if (document.readyState !== 'loading') {
+	eventHandler();
+} else {
+	document.addEventListener('DOMContentLoaded', eventHandler);
+}
+
+// window.onload = function () {
+// 	document.body.classList.add('loaded_hiding');
+// 	window.setTimeout(function () {
+// 		document.body.classList.add('loaded');
+// 		document.body.classList.remove('loaded_hiding');
+// 	}, 500);
+// }
