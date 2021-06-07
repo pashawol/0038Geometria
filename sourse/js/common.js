49af4304f32505eb216d3112c5451bd133b1c4c3
Pavel <wol1414@gmail.com>
2021-06-07 21:06:26 +0300

    add new  if

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -265,7 +265,75 @@ function eventHandler() {
 			}
 		}
 	});
+	const swiperStep = new Swiper('.sSteps__slider--js', {
+		// slidesPerView: 5,
+		...defaultSl,
+		slidesPerView: 1,
+		spaceBetween: 20, 
+		loop: false,
+		breakpoints: {
+			768: {
+				slidesPerView: 'auto',
+				spaceBetween: 0, 
+			}
+		}
+	});
 	// modal window
+
+
+	function getTimeRemaining(endtime) {
+		const total = Date.parse(endtime) - Date.parse(new Date());
+		const seconds = Math.floor((total / 1000) % 60);
+		const minutes = Math.floor((total / 1000 / 60) % 60);
+		const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+		const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+		return {
+			total,
+			days,
+			hours,
+			minutes,
+			seconds
+		};
+	}
+
+	const clock = document.querySelector('.timer-block--js');
+	if (clock) { 
+		function initializeClock(endtime) {
+			const daysSpan = clock.querySelector('.days');
+			const hoursSpan = clock.querySelector('.hours');
+			const minutesSpan = clock.querySelector('.minutes');
+			// const secondsSpan = clock.querySelector('.seconds');
+
+			function updateClock() {
+				const t = getTimeRemaining(endtime);
+
+				daysSpan.innerHTML = t.days;
+				hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+				minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+				// secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+				if (t.total <= 0) {
+					clearInterval(timeinterval);
+				}
+			}
+
+			updateClock();
+			const timeinterval = setInterval(updateClock, 1000);
+		}
+
+		// const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+
+		const deadline = new Date(Date.parse(clock.dataset.date));
+		console.log(deadline);
+		initializeClock( deadline);
+	}
+
+	$(".accordion-item__head").click(function(){
+		$(this).next().slideToggle(function(){
+			$(this).parent().toggleClass("active");
+		})
+	})
 
 };
 if (document.readyState !== 'loading') {
